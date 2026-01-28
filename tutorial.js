dc4472a013f6aa7b0a4935bcd837505a5390c31e
JerryOffiicial <jerrysonjerry1234@email.com>
2026-01-28 19:34:59 +0530

    first commit

---
+++ tutorial.js
@@ -0,0 +1,11 @@
+var id = 5;
+var company = "Acne Corp";
+var isPublished = true;
+var ids = [1, 2, 3, 4, 5];
+var x = "pedro";
+var xArr = ["pedro", 0, true];
+var concatenateValues = function (a, b) {
+    return a + b;
+};
+console.log(concatenateValues("Hello", "world"));
+console.log(concatenateValues("5", "10"));
